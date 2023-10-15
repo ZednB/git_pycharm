cc0c9243544e03580bcc3e8cdb9ce3264dadca19
Zubayr Bibulatov <Ru.Zubayr@gmail.com>
2023-10-15 11:06:48 +0300

    new file1

--- functions.py
+++ functions.py
@@ -1,3 +1,8 @@
 def function():
     n = 2 + 3
     return n
+
+
+def function2():
+    n = 3 - 2
+    return n
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 from functions import function
+from functions import function2
+
 
 print(function())
+print(function2())
